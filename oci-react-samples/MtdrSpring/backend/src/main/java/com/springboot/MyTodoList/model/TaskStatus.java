0b8a75462d74f7249b13cfc5885fa8fb8e1e9b5d
samgbsr <sam.garcia.96@gmail.com>
2024-05-02 19:53:13 -0600

    Task Status Update

--- oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/TaskStatus.java
+++ oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/TaskStatus.java
@@ -1,62 +1,102 @@
 package com.springboot.MyTodoList.model;
 
-
-import java.util.List;
-
 import javax.persistence.*;
+
+import org.hibernate.usertype.UserType;
+
 @Entity
 @Table(name = "TASKSTATUS")
-public class TaskStatus {
+public class TASKSTATUS{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "TaskStatusId")
-    int ID;
+    int TaskId;
     @Column(name = "Name")
-    String name;
+    String Name;
     @Column(name = "Description")
-    String description;
-    @OneToMany(mappedBy = "TaskStatusId", cascade = CascadeType.ALL)
-    List<Task> tasks;  
-    public TaskStatus(){
-    }
-    public TaskStatus(int ID, String name ,String description) {
-        this.ID = ID;
-        this.name = name;
-        this.description = description;
-    }
+    String Description;
+    @Column(name = "EstimatedHours")
+    float EstimatedHours;
+    @Column(name = "Priority")
+    int Priority;
+    //i 
+    int UserId;
+    @ManyToOne
+    @JoinColumn(name = "UserId", referencedColumnName = "UserId")
+    private int user;
 
-    public int getID() {
-        return ID;
-    }
+    @ManyToOne
+    @JoinColumn(name = "SprintId", referencedColumnName = "SprintId")
+    private int sprint;
 
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-    public String getName() {
-        return name;
-    }
+    @ManyToOne
+    @JoinColumn(name = "TaskStatusId", referencedColumnName = "TaskStatusId")
+    private int taskStatus;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @ManyToOne
+    @JoinColumn(name = "UserTypeId", referencedColumnName = "UserTypeId")
+    private UserType userType;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    @ManyToOne
+    @JoinColumn(name = "UserTeamId", referencedColumnName = "UserTeamId")
+    private int userTeam;
     
-    public List<Task> getTasks(){
-        return tasks;
-    }
-    @Override
-    public String toString() {
-        return "TaskStatus{" +
-                "ID=" + ID +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
+
+ public TASKSTATUS(int TaskId, String Name, String Description, float EstimatedHours, int Priority) {
+     this.TaskId = TaskId;
+     this.Name = Name;
+     this.Description = Description;
+     this.EstimatedHours = EstimatedHours;
+ }
+ 
+ public int getTaskId() {
+     return TaskId;
+ }
+ 
+ public void setTaskId(int TaskId) {
+     this.TaskId = TaskId;
+ }
+ 
+ public String getName() {
+     return Name;
+ }
+ 
+ public void setName(String Name) {
+     this.Name = Name;
+ }
+ 
+ public String getDescription() {
+     return Description;
+ }
+ 
+ public void setDescription(String Description) {
+     this.Description = Description;
+ }
+ 
+ public float getEstimatedHours() {
+     return EstimatedHours;
+ }
+ 
+ public void setEstimatedHours(float EstimatedHours) {
+     this.EstimatedHours = EstimatedHours;
+ }
+ 
+ public int getPriority() {
+     return Priority;
+ }
+ 
+ public void setPriority(int Priority) {
+     this.Priority = Priority;
+ }
+ 
+ @Override
+ public String toString() {
+     return "TASKSTATUS{" +
+             "TaskId=" + TaskId +
+             ", Name='" + Name + '\'' +
+             ", Description='" + Description + '\'' +
+             ", EstimatedHours=" + EstimatedHours +
+             ", Priority=" + Priority +
+             '}';
+ }
 }
+
+
